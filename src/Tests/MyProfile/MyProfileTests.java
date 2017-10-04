3dd62bd5a2e219a1812f755683724a833594db34
yanagusti <yana.gusti@gmail.com>
2017-10-04 17:17:10 +0300

    Login, Logout
    Change password
    
    Create, change and delete organization

--- src/Tests/MyProfile/MyProfileTests.java
+++ src/Tests/MyProfile/MyProfileTests.java
@@ -10,25 +10,25 @@ import org.testng.annotations.Test;
  */
 public class MyProfileTests extends BaseTest{
 
-    @Test
-    public void changeBillingInfo() throws InterruptedException {
-        Login login = new Login();
-        login.login(driver,email, password);
-        waitForElement(".//*[@id='mCSB_2_container']/div[1]/div/div/div/div/div[1]/div[1]/a");
-        MyProfile myProfile = login.goToMyProfile(driver);
-        waitForElement(".//*[@id='edit_user_543']/div[1]/h3");
-        myProfile.changeBillingInfo(driver,"test", "test", "test","test","12312", "test", "Albania");
-        waitForElement("html/body/div[2]/div[1]");
-        myProfile.checkText(driver,"Update Success", "html/body/div[2]/div[1]");
-        myProfile.clickButton(driver, "html/body/div[2]/div[2]/span");
-        myProfile.checkText(driver,"test", ".//*[@id='user_company']");
-        myProfile.changeBillingInfo(driver,"test1", "test1", "test1","test1","12311", "test1", "Australia");
-        waitForElement("html/body/div[2]/div[1]");
-        myProfile.checkText(driver,"Update Success", "html/body/div[2]/div[1]");
-        myProfile.clickButton(driver, "html/body/div[2]/div[2]/span");
-        myProfile.checkText(driver,"test1", ".//*[@id='user_company']");
-        login.logout(driver);
-    }
+//    @Test
+//    public void changeBillingInfo() throws InterruptedException {
+//        Login login = new Login();
+//        login.login(driver,email, password);
+//        waitForElement(".//*[@id='mCSB_2_container']/div[1]/div/div/div/div/div[1]/div[1]/a");
+//        MyProfile myProfile = login.goToMyProfile(driver);
+//        waitForElement(".//*[@id='edit_user_543']/div[1]/h3");
+//        myProfile.changeBillingInfo(driver,"test", "test", "test","test","12312", "test", "Albania");
+//        waitForElement("html/body/div[2]/div[1]");
+//        myProfile.checkText(driver,"Update Success", "html/body/div[2]/div[1]");
+//        myProfile.clickButton(driver, "html/body/div[2]/div[2]/span");
+//        myProfile.checkText(driver,"test", ".//*[@id='user_company']");
+//        myProfile.changeBillingInfo(driver,"test1", "test1", "test1","test1","12311", "test1", "Australia");
+//        waitForElement("html/body/div[2]/div[1]");
+//        myProfile.checkText(driver,"Update Success", "html/body/div[2]/div[1]");
+//        myProfile.clickButton(driver, "html/body/div[2]/div[2]/span");
+//        myProfile.checkText(driver,"test1", ".//*[@id='user_company']");
+//        login.logout(driver);
+//    }
 
     @Test
     public void changePassword(){
